27a32bd79889f4dec7760e6b205b1d061d96d753
Jayakrushna35 <jayakrushnapattnaik6@gmail.com>
2025-01-25 11:01:07 +0530

    add book model

---
+++ book/bookController.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import createHttpError from 'http-errors';
+
+
+const createBook = async(req:Request,res:Response,next:NextFunction)=>{
+   
+};
+
+
+export {createBook};
