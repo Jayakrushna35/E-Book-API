3030d949bb7ad5cd9106fa6921a28aded21ef90a
Jayakrushna35 <jayakrushnapattnaik6@gmail.com>
2025-01-28 11:39:27 +0530

    add book list point

--- book/bookController.ts
+++ book/bookController.ts
@@ -126,6 +126,13 @@ const updateBook = async(req:Request,res:Response,next:NextFunction)=>{
     );
 
 }; 
+ const listBooks = async(req:Request,res:Response,next:NextFunction)=>{
+  try{
+    const book = await bookModel.find();
+    res.json(book);
+  }catch(err){
+    return next(createHttpError(500,"Error while getting a book"))
+  }
+ };
 
-
-export {createBook,updateBook};
+export {createBook,updateBook,listBooks};
--- book/bookRouter.ts
+++ book/bookRouter.ts
@@ -1,5 +1,5 @@
 import  express  from "express";
-import { createBook, updateBook } from "./bookController";
+import { createBook, listBooks, updateBook } from "./bookController";
 import multer from "multer";
 import path from "node:path";
 import authenticate from "../src/middleware/authenticate";
@@ -19,6 +19,8 @@ bookRouter.patch("/:bookId",authenticate, upload.fields([
     { name: 'converImage', maxCount: 1 },
     { name: 'file', maxCount: 1 }
 ]), updateBook);
+
+bookRouter.get("/",listBooks);
 
 export default bookRouter;
 
